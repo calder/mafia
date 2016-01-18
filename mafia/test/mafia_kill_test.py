df1057fe900ef18bad66f1278be578d1e05bda62
Calder Coalson <caldercoalson@gmail.com>
2016-01-18 10:35:24 -0800

    Added double kill rejection test.

--- mafia/test/mafia_kill_test.py
+++ mafia/test/mafia_kill_test.py
@@ -4,21 +4,37 @@ from .test_game import TestGame
 from nose_parameterized import parameterized
 from unittest import TestCase
 
-def test_mafia_kill():
-  game = TestGame()
-  town  = game.add_faction(Town())
-  mafia = game.add_faction(Mafia("Mafia"))
-  villager = game.add_player(Player("Villager", role=Villager(faction=town)))
-  goon     = game.add_player(Player("Goon", role=Goon(faction=mafia)))
+class MafiaKillTest(TestCase):
+  def setUp(self):
+    self.game = TestGame()
+    self.town  = self.game.add_faction(Town())
+    self.mafia = self.game.add_faction(Mafia("Mafia"))
+    self.villager1 = self.game.add_player(Player("Villager 1", role=Villager(faction=self.town)))
+    self.villager2 = self.game.add_player(Player("Villager 2", role=Villager(faction=self.town)))
+    self.goon1     = self.game.add_player(Player("Goon 1", role=Goon(faction=self.mafia)))
+    self.goon2     = self.game.add_player(Player("Goon 2", role=Goon(faction=self.mafia)))
 
-  night0 = Night(0)
-  night0.add_action(FactionAction(mafia, Kill(goon, villager)))
+  def test_mafia_kill(self):
+    night0 = Night(0)
+    night0.add_action(FactionAction(self.mafia, Kill(self.goon1, self.villager1)))
+    self.game.resolve(night0)
 
-  assert villager.alive
-  game.resolve(night0)
-  assert not villager.alive
+    assert self.villager1.alive is False
+    assert_equal(self.game.log, Log([
+      Visited(self.goon1, self.villager1),
+      Died(self.villager1),
+    ], phase=night0))
 
-  assert_equal(game.log, Log([
-    Visited(goon, villager),
-    Died(villager),
-  ], phase=night0))
+  def test_reject_multiple_mafia_kills(self):
+    """Only the last kill should go through."""
+    night0 = Night(0)
+    night0.add_action(FactionAction(self.mafia, Kill(self.goon1, self.villager1)))
+    night0.add_action(FactionAction(self.mafia, Kill(self.goon2, self.villager2)))
+    self.game.resolve(night0)
+
+    assert self.villager1.alive is True
+    assert self.villager2.alive is False
+    assert_equal(self.game.log, Log([
+      Visited(self.goon2, self.villager2),
+      Died(self.villager2),
+    ], phase=night0))
